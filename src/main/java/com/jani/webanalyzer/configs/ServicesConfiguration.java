57829fb643f704f00e00d7b3deef7a03875ba731
Jacek Niedzwiecki <j.niedzwiecki@oberthur.com>
2017-03-12 00:43:52 +0100

    Added logging aspect.

--- src/main/java/com/jani/webanalyzer/configs/ServicesConfiguration.java
+++ src/main/java/com/jani/webanalyzer/configs/ServicesConfiguration.java
@@ -1,12 +1,15 @@
 package com.jani.webanalyzer.configs;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.jani.webanalyzer.aspects.LoggingAspect;
+import com.jani.webanalyzer.services.WebAnalyzer;
 import com.jani.webanalyzer.services.WebAnalyzerService;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,21 +18,27 @@ import java.util.List;
  * Created by jacekniedzwiecki on 05.03.2017.
  */
 @Configuration
+@EnableAspectJAutoProxy
 public class ServicesConfiguration {
 
     private static final String WEB_SERVICE_ADDRESS = "http://0.0.0.0:8080";
 
     @Bean
     @Autowired
-    public Server rsServer(WebAnalyzerService webAnalyzerService) {
+    public Server rsServer(WebAnalyzer webAnalyzer) {
         JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
         endpoint.setAddress(WEB_SERVICE_ADDRESS);
-        endpoint.setServiceBean(webAnalyzerService);
+        endpoint.setServiceBean(webAnalyzer);
 
         List<Object> providers = new ArrayList<>();
         providers.add(new JacksonJaxbJsonProvider());
         endpoint.setProviders(providers);
 
         return endpoint.create();
+    }
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
     }
 }
